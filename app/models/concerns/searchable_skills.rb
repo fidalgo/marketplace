c0b88c87376435bf9f8cb2239516de780a9d3552
Paulo Fidalgo <fidalgo@gmx.com>
2015-11-25 14:42:45 +0000

    Allow to search with multiple skills
    Allow multiple values in the search query
    Add the search bar to the results

--- app/models/concerns/searchable_skills.rb
+++ app/models/concerns/searchable_skills.rb
@@ -2,8 +2,8 @@ module SearchableSkills
   extend ActiveSupport::Concern
 
   module ClassMethods
-    def find_workers_by_skill_name(skill_name)
-      skill_ids = Skill.where('name ILIKE ?', "%#{skill_name}%").pluck(:id)
+    def find_workers_by_skill_name(skills) # quando tenho 2 skills falha
+      skill_ids = skill_ids(skills)
       worker_ids = Skilling.where(skill: skill_ids).pluck(:worker_id)
       Worker.uniq.includes(:skills).find(worker_ids)
     end
@@ -14,6 +14,17 @@ module SearchableSkills
       else
         Skill.uniq.pluck(:name)
       end
+    end
+
+    private
+
+    def skill_ids(skills)
+      return Skill.uniq.pluck(:id) unless skills.present?
+      Skill.where('name ILIKE ANY ( array[?] )', sql_values(skills)).pluck(:id)
+    end
+
+    def sql_values(skills)
+      skills.split(',').map { |skill| "%#{skill.strip}%" } if skills.present?
     end
   end
 end
